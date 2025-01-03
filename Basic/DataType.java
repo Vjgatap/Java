5e1e568e756b89431c597214057e368389a79bd6
Vallabh Jagtap <95693051+Vjgatap@users.noreply.github.com>
2025-01-03 20:14:19 +0000

    Setup is ready and Basic

---
+++ Basic/DataType.java
@@ -0,0 +1,14 @@
+public class DataType{
+    public static void main(String args []){
+        // data type in java
+        int num = 44;
+        float num2 = 4.4f;
+        double pi = 3.14;
+        char alphabate = 'v';
+
+        System.out.println("Num : "+num);
+        System.out.println("Num2 : "+num2);
+        System.out.println("Double : "+pi);
+        System.out.println("char : "+alphabate);
+    }
+}
