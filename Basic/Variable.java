65303793b9a89148e51437e93755dd6868e323f0
vallabh <vallabhjagtap39@gmail.com>
2025-06-12 09:51:50 +0530

    Varibales

---
+++ Basic/Variable.java
@@ -0,0 +1,9 @@
+
+public class Variable {
+    public static void main(String args[]) {
+
+        int x = 10;
+        System.out.println("The varible is x and the datatype is int and value is : " + x);
+
+    }
+}
