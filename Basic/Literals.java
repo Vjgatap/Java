09047d9478fe726e68938744f0c3c6ac9bb5e544
Vallabh Jagtap <95693051+Vjgatap@users.noreply.github.com>
2025-06-12 04:44:53 +0000

    Literals in java

---
+++ Basic/Literals.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Literals{
+	public static void main(String args []){
+
+	byte b1 = 10;
+	byte b2 = 0b1010;
+	byte b3 = 012;
+	byte b4 = 0xA;
+	
+	System.out.println(b1);
+	System.out.println(b2);
+	System.out.println(b3);
+	System.out.println(b4);
+
+	}
+
+
+
+}
